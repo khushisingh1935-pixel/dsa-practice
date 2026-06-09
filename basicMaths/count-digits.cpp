c5552c2330ab6b798c0db37918992d9899459d58
Khushi Singh <khushisingh1935@gmail.com>
2026-06-09 23:48:18 +0530

    Initial

---
+++ basicMaths/count-digits.cpp
@@ -0,0 +1,14 @@
+int count(int n){
+    int count = 0; 
+    while(n>0){
+        int lastDigit = n%10;
+        count++;
+        n = n/10;
+    }
+    return count;
+}
+// ALTERNATIVE WAY 
+int count(int n){
+    int count = (int)(log10(n) + 1);
+    return count++;
+}
